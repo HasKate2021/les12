a158055ffd7805d7c78573276decfbfb63dec319
Kit <simkatesi@yandex.ru>
2023-05-02 15:16:52 +0400

    1

---
+++ data.js
@@ -0,0 +1,63 @@
+export const dataProducts = `[
+    {
+        "id": 1,
+        "name": "mango people t-shirt",
+        "image": "https://i.postimg.cc/T15XPMmF/Rectangle-15-copy.jpg",
+        "text": "Optio, explicabo aliquid sed, cumque fugit ipsam id quia eaque architecto distinctio earum, omnis delectus.",
+        "price": 300,
+        "color":"blue",
+        "size":"XXL",
+        "quantity":2
+    },
+    {
+        "id": 2,
+        "name": "ellery x m'o capsule",
+        "image": "img/catalog-item2.png",
+        "text": "Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": 52,
+        "color":"black",
+        "size":"L",
+        "quantity":2
+    },
+    {
+        "id": 3,
+        "name": "blue cubic t-shirt",
+        "image": "img/catalog-item3.jpg",
+        "text": "Shop for items based on what we featured in this week.",
+        "price": 200,
+        "color":"blue",
+        "size":"M",
+        "quantity":2
+    },
+    {
+        "id": 4,
+        "name": "yellow summer pants",
+        "image": "img/catalog-item4.jpg",
+        "text": "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+        "price": 150,
+        "color":"yellow",
+        "size":"M",
+        "quantity":2
+    },
+    {
+        "id": 5,
+        "name": "blue light jacket",
+        "image": "img/catalog-item5.jpg",
+        "text": "Placeat possimus nostrum tempora, necessitatibus quibusdam.",
+        "price": 70,
+        "color":"sky",
+        "size":"XS",
+        "quantity":2
+    },
+    {
+        "id": 6,
+        "name": "striped blouse",
+        "image": "img/catalog-item6.jpg",
+        "text": "laborum perspiciatis voluptas reiciendis itaque.",
+        "price": 135,
+        "color":"grey",
+        "size":"S",
+        "quantity":2
+    }
+    
+]`;
